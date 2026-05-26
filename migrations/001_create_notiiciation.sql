44f32803502f451e2edb79d45ec2b446a41a2d5c
Sujal <web.sujal@gmail.com>
2026-05-26 23:09:46 +0530

    feat: Add migration script and update notifications table schema
    - Introduced a new migration script to handle database migrations.
    - Updated the notifications table schema to include title, content, is_seen, and created_at fields.
    - Added a new script command for running migrations in package.json.

--- migrations/001_create_notiiciation.sql
+++ migrations/001_create_notiiciation.sql
@@ -1,3 +1,7 @@
-CREATE TABLE notifications {
+CREATE TABLE IF NOT EXISTS notifications {
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  title TEXT NOT NULL,
+  content TEXT NOT NULL,
+  is_seen BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
 }
---
+++ src/db/migrate.ts
@@ -0,0 +1,58 @@
+import fs from "fs";
+import path from "path";
+
+import { sql } from "../config/db.js";
+import logger from "../config/logger.js";
+
+const runMigration = async (): Promise<void> => {
+  logger.info("🚀 Starting database migrations...");
+
+  try {
+    // 1. Create tracking table if not exists
+    await sql`CREATE TABLE IF NOT EXISTS schema_migrations (
+      version TEXT NOT NULL PRIMARY KEY,
+      executed_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    )`;
+
+    // 2. Read and sort migration files from migrations directory
+    const migrationsDir = path.join(process.cwd(), "migrations");
+    const files = fs.readdirSync(migrationsDir);
+    const sqlFiles = files.filter((file) => file.endsWith(".sql")).sort();
+
+    // 3. Execute each migration file that hasn't been applied
+    for (const file of sqlFiles) {
+      const version = file.replace(".sql", "");
+
+      const [alreadyExecuted] = await sql`
+        SELECT version FROM schema_migrations WHERE version = ${version}
+        `;
+
+      if (alreadyExecuted) {
+        logger.info(`⏩ Skipping ${version} (already executed)...`);
+        continue;
+      }
+
+      logger.info(`⚙️ Executing ${version}...`);
+
+      const filePath = path.join(migrationsDir, file);
+      const sqlContent = fs.readFileSync(filePath, "utf8");
+
+      // Execute the raw sql inside a transaction
+      await sql.begin(async (tx) => {
+        await tx.unsafe(`${sqlContent}`);
+        await tx`INSERT INTO schema_migrations (version) VALUES (${version})`;
+      });
+
+      logger.info(`✅ Successfully executed ${version}...`);
+    }
+
+    logger.info("✨ All migrations completed successfully!");
+  } catch (err) {
+    logger.error("❌ Failed to run migrations:", err);
+    process.exit(1);
+  } finally {
+    await sql.end({ timeout: 5 });
+  }
+};
+
+runMigration();
