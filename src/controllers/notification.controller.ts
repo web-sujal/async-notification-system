88abfde69c8ccb3e940709e5d5053d31fd24112e
Sujal <web.sujal@gmail.com>
2026-05-27 01:02:41 +0530

    feat: Enhance notification creation with logging

--- src/controllers/notification.controller.ts
+++ src/controllers/notification.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import logger from "../config/logger.js";
 import { notificationService } from "../services/index.js";
 import { sendData } from "../utils/apiSuccess.js";
 
 export const createNotification = async (req: Request, res: Response) => {
-  const result = await notificationService.createNotification(req.body);
+  const notification = await notificationService.createNotification(req.body);
 
-  sendData(res, result, StatusCodes.CREATED);
+  logger.info(`Notification created: ${notification.id}`);
+
+  sendData(res, notification, StatusCodes.CREATED);
   return;
 };
